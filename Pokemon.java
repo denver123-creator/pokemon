0d1e4d95b77eb96424d1a3e5b8a6f80c7baa7cb0
denver123-creator <59923346+denver123-creator@users.noreply.github.com>
2024-01-31 05:21:02 -0800

    Create Pokemon.java

---
+++ Pokemon.java
@@ -0,0 +1,11 @@
+public class Pokemon {
+      String pokemonElementType;
+      boolean isEvolve;
+
+    Pokemon(String pokemonElementType, boolean isEvolve){
+        this.pokemonElementType = pokemonElementType;
+        this.isEvolve = isEvolve;
+    }
+}
+
+
